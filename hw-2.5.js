e27c1cf1737e447d08fda97725acb52b6e3d83ad
rambak02 <rambak02@mail.ru>
2023-03-16 01:07:53 +0700

    Добавил игру 'Времена года'

--- hw-2.5.js
+++ hw-2.5.js
@@ -77,3 +77,21 @@ const circle2 = {
   getArea,
   getPerimeter,
 };
+// Задание 9
+function gameSeasons() {
+  let seasNum = parseInt(prompt("Введите число"));
+  if (!isNaN(seasNum) && seasNum > 0 && seasNum < 13) {
+    if ((seasNum > 0 && seasNum <= 2) || seasNum == 12) {
+      alert("Зима");
+    } else if (seasNum > 2 && seasNum <= 5) {
+      alert("Весна");
+    } else if (seasNum > 5 && seasNum <= 8) {
+      alert("Лето");
+    } else if (seasNum > 8 && seasNum <= 11) {
+      alert("Осень");
+    }
+  } else {
+    alert("Вы ввели не число, ввели ноль или больше 12");
+  }
+}
+gameSeasons();
